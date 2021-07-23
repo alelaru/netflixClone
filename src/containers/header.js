c6b48df1e23d1846b6a9518b11d79b488aa081ab
Alejandro Lara <alelaru@gmail.com>
2021-07-23 16:20:01 +0200

    Start working on the Header component

---
+++ src/containers/header.js
@@ -0,0 +1,14 @@
+import { Header } from "../components/acoordeon/styles/acoordeon"
+
+
+const HeaderContainer = () => {
+    return ( 
+        <Header>
+            <Header.Title>
+                
+            </Header.Title>
+        </Header>
+     );
+}
+ 
+export default HeaderContainer;
