2d39c8f4a20e5d3dbea9940cc1a5891ca819e5a1
Alejandro Lara <alelaru@gmail.com>
2021-07-29 23:13:48 +0200

    First part of header done, now lets work on the feature

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,11 +1,12 @@
 import Header  from "../components/header"
 import * as ROUTES from "../constants/routes"
+import logo from "../logo.svg"
 
 export const HeaderContainer = ( {children }) => {
     return ( 
         <Header>
             <Header.Frame>
-                <Header.Logo />
+                <Header.Logo to={ROUTES.HOME} alt="Netflix" src={logo} />
                 <Header.ButtonLink to={ROUTES.SIGNIN}>Sign In</Header.ButtonLink>
             </Header.Frame>
             {children}
--- src/pages/home.js
+++ src/pages/home.js
@@ -7,16 +7,23 @@ import { JumbotronContainer } from "../containers/jumbotron"
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import * as ROUTES from "../constants/routes"
 import {HeaderContainer} from '../containers/header';
+import OptForm from "../components/opt-form"
+
 
 export default function Home() {
   return (
-    <Router>
-      <Route exact path={ROUTES.HOME}>
-        <HeaderContainer />
+    <>
+        <HeaderContainer>
+        <OptForm>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+          </OptForm>
+        </HeaderContainer>
         <JumbotronContainer></JumbotronContainer>
         <FaqsContainer></FaqsContainer>
         <FooterContainer></FooterContainer>
-      </Route>
-    </Router>
+    </>
   );
 }
