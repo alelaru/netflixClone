a7bef0c2019f9224cd065ad9860829f22dd09a2b
Alejandro Lara <alelaru@gmail.com>
2021-08-04 12:16:24 +0200

    Working on the Cards once they are clicked we can see the extra information this part is WOP

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import { Container, Group, Title, SubTitle, Text, Feature, FeatureTItle, FeatureText, FeatureClose, Maturity, Content, Meta, Entities, Item, Image  } from "./styles/card";
+import { Container, Group, Title, SubTitle, Text, Feature, FeatureTitle, FeatureText, FeatureClose, Maturity, Content, Meta, Entities, Item, Image  } from "./styles/card";
 
 export const FeatureContext = createContext();
 
@@ -37,6 +37,32 @@ Card.Meta = function CardMeta({children, ...restProps}){
 
 Card.Entities = function CardEntities({children, ...restProps}){
     return <Entities {...restProps}>{children}</Entities>
+}
+
+Card.Feature = function CardFeature({children, category, ...restProps}){
+    const {showFeature, itemFeature,  setShowFeature} = useContext(FeatureContext)
+
+
+
+    return showFeature 
+    ? <Feature 
+        {...restProps}
+        src={`images/category/${itemFeature.genre}/${itemFeature.slug}/large.jpg`}>
+            <Content>
+                <FeatureTitle>{itemFeature.title}</FeatureTitle>
+                <FeatureText>{itemFeature.description}</FeatureText>
+                <FeatureClose onClick={() => setShowFeature(false)}>
+                    <img src={"images/icons/close.png"} alt="Close"></img>
+                </FeatureClose>
+            </Content>
+            <Group margin="30px 0" flexDirection="row" alignItems="center">
+                <Maturity rating={itemFeature.maturity}>{itemFeature.maturity < 12 ? "PG" : itemFeature.maturity}</Maturity>
+                <FeatureText fontWeight="bold">
+                    {itemFeature.genre.charAt(0).toUpperCase() + itemFeature.genre.slice(1)}
+                </FeatureText>
+            </Group>
+    </Feature>
+    : null
 }
 
 
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -70,7 +70,30 @@ const BrowserContainer = ( {slides }) => {
                 </Header>
 
                 <Card.Group>
-
+                    {slideRows.map((slideItem) => (
+                        <Card  key={`${category}-${slideItem.title.toLowerCase()}`}>
+                            <Card.Title>{slideItem.title}</Card.Title>
+                            <Card.Entities>
+                                {slideItem.data.map((item) => (
+                                   <Card.Item key={item.docId} item={item}>
+                                       <Card.Image 
+                                        src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}></Card.Image>
+                                    <Card.Meta>
+                                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                                        <Card.Text>{item.description}</Card.Text>
+                                    </Card.Meta>
+                                   </Card.Item> 
+                                ))}
+                            </Card.Entities>
+                            <Card.Feature category={category}>
+                                <p>Hello</p>
+                                {/* <Player>
+                                    <Player.Button></Player.Button>
+                                    <Player.Video src="/videos/bunny.mp4"></Player.Video>
+                                </Player> */}
+                            </Card.Feature>
+                        </Card>
+                    ))}
                 </Card.Group>
             </>
         ) : <SelectProfileContainer user={user} setProfile={setProfile}></SelectProfileContainer>
